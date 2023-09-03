179073f563a3d2c3be3e9fab4d3d7669ef74e88d
Seemanta Sarker <91196514+StrangerSeemanta@users.noreply.github.com>
2023-09-04 01:13:00 +0600

    first comment

---
+++ src/App.tsx
@@ -0,0 +1,33 @@
+import React, { Fragment, Component, useLayoutEffect, useRef, useState } from 'react';
+import Navbar from './components/Navbar';
+import Intro from './components/Intro';
+import About from './components/About';
+import Skill from './components/Skill';
+import Projects from './components/Projects';
+import './App.css';
+
+
+function App() {
+
+  const links = ['Home', "About", "Skill", "Projects", "Contact"];
+  const handleNavClick = (item: string, index: number) => {
+    window.location.href = `#${item.toLocaleLowerCase()}`
+  }
+  const logoDetails = {
+    src: '/src/assets/logo.png',
+    altText: "Logo.png"
+  }
+
+
+  return (
+    <Fragment>
+      <Navbar logo={logoDetails} links={links} onSelected={handleNavClick} />
+      <Intro />
+      <About />
+      <Skill />
+      <Projects />
+    </Fragment >
+  )
+}
+
+export default App
